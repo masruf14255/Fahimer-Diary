0ef61aa3990a0a582079f33cac7d2a4b774db097
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-07-07 21:04:40 +0600

    Create Basic C++.cpp

---
+++ Basic C++.cpp
@@ -0,0 +1,116 @@
+প্রথম অংশ - সি++ নিয়ে বকরবকর
+
+স্ট্রাকচার->
+
+struct data
+{
+	char name[20];
+	int number,email_num;
+};
+
+struct day{
+	data new_people[100];
+	int total_new_people;
+	worklist todo[100];
+};
+
+when my mam give me gada gada math->>>>
+
+struct day{
+	data new_people[100];
+	int total_new_people;
+	worklist todo[100];
+
+
+
+
+	void charm_your_miss()
+	{
+		while(true)do_math();
+		while(true)smile();
+	}
+
+	void do_math()
+	{
+		itn a,b;
+		cin>>a>>b;
+		cout<<a+b<<endl;
+	}
+	void smile()
+	{
+		cout<<" :) "<<endl;
+	}
+
+};
+
+int main()
+{
+	day a_bright_new_day,mora_day;
+	mora_day.total_new_people=0;
+	return 0;
+}
+
+
+int main()
+{
+	day a_bright_new_day,mora_day;
+	while(true)a_bright_new_day.smile();
+	return 0;
+}
+
+//chiching bondhooooooooo
+
+struct day
+{
+	data new_people[100];
+	int total_new_people;
+	worklist todo[100];
+
+
+private:
+	sweetthings bolbo_keno[100];
+};
+
+
+//chiching fakkkkkkkkkk
+
+
+struct day
+{
+	data new_people[100];
+	int total_new_people;
+	worklist todo[100];
+
+
+public:
+	sweetthings bolbo_keno[100];
+};
+
+
+/// class murobbi here 
+
+class day
+{
+	data new_people[100];
+	int total_new_people;
+	worklist todo[100];
+
+
+private:
+	sweetthings bolbo_keno[100];
+};
+
+
+// absolute value 
+
+int absolute_int( int x ) {
+    if( x < 0 ) return -x;
+    else return x;
+}
+
+-----------------------------
+
+long long absolute_ll( long long x ) {
+    if( x < 0 ) return -x;
+    else return x;
+}
